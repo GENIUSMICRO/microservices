4988c2aa5a6f22c9b5f545f66cb999357ff47aac
thisisowais <mohdowaisansari608@gmail.com>
2024-03-27 12:54:59 +0530

    adding feature 123

--- customer/src/services/customer-service.test.js
+++ customer/src/services/customer-service.test.js
@@ -1,6 +1,6 @@
 // which service it is
 describe("CustomerService", () => {
-  // This will explain which function we are testing for customer service!
+  // This will explain which function we are testing for services of customer!
   describe("SignIn", () => {
     // Which Scenario we are testing
     test("validate user inputs", () => {});
